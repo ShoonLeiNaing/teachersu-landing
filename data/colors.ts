c24f6882f7266499d841250748a41fa430445efc
ShoonLeiNaing <pandapanda254.pp@gmail.com>
2022-11-04 22:07:51 +0630

    initial prj setup

---
+++ data/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  red: "#BD2122",
+  blue: "#0F3B7A",
+  white: "#F6F9FE",
+  paleGrey: "#E0E0E0",
+  black: "#333333",
+  darkGrey: "#737373",
+  grey: "#9C9C9C",
+  lightblue: "#3B8CF7",
+  pink: "#FF7FEB",
+  yellow: "#FFD84C",
+  green: "#47F09F",
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
